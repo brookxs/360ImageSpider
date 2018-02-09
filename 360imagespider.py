9856e00bae2e1eac450c4dd142eafae68589d91b
brooks <76231607@qq.com>
2018-02-09 09:46:52 +0800

    add some comment

--- 360imagespider.py
+++ 360imagespider.py
@@ -9,6 +9,7 @@ import os
 import time
 import requests
 
+# 自定义请求头
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36"
                   " (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36"
@@ -97,3 +98,4 @@ if __name__ == "__main__":
         time.sleep(2)
 
     print(u"全部采集完毕")
+
